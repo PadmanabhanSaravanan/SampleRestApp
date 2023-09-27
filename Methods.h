169357b46e2bf3b950b875eeefd6e1abf94203a1
PadmanabhanSaravanan <g.s.r.padmanabhan@gmail.com>
2023-09-27 09:56:26 +0530

    update,delete,getbyid method updated

--- Methods.h
+++ Methods.h
@@ -17,36 +17,73 @@ using bsoncxx::builder::basic::make_document;
 // Create a todo from the given key-value pairs.
 bsoncxx::document::value createTodo(const vector<pair<string, string>>& keyValues)
 {
-	bsoncxx::builder::stream::document document{};
-	for (auto& keyValue : keyValues)
-	{
-		document << keyValue.first << keyValue.second;
-	}
-	return document << bsoncxx::builder::stream::finalize;
+    bsoncxx::builder::stream::document document{};
+    for (auto& keyValue : keyValues)
+    {
+        document << keyValue.first << keyValue.second;
+    }
+    return document << bsoncxx::builder::stream::finalize;
 }
 
 // Add the todo to the given collection.
 void insertTodo(mongocxx::collection& collection, const bsoncxx::document::value& document)
 {
-	collection.insert_one(document.view());
+    collection.insert_one(document.view());
 }
 
 // Find a todo from the given key-value pairs and return true if found.
 bool findTodo(mongocxx::collection& collection, const string& key, const string& value)
 {
-	// Create the query filter
-	auto filter = bsoncxx::builder::stream::document{} << key << value << bsoncxx::builder::stream::finalize;
-	//Add query filter argument in find
-	auto cursor = collection.find({ filter });
-	auto count = std::distance(cursor.begin(), cursor.end());
-	if (count != 0L) {
-		return true;
-	}
-	return false;
+    // Create the query filter
+    auto filter = bsoncxx::builder::stream::document{} << key << value << bsoncxx::builder::stream::finalize;
+    //Add query filter argument in find
+    auto cursor = collection.find({ filter });
+    auto count = std::distance(cursor.begin(), cursor.end());
+    if (count != 0L) {
+        return true;
+    }
+    return false;
 }
 
 //Pass the given collection and key-value pairs.
 bool findTodoRecord(mongocxx::collection& collection, const string& id)
 {
-	return findTodo(collection, "Id", id);
+    return findTodo(collection, "Id", id);
+}
+
+// Update a todo in the given collection based on a specific key-value pair with new key-value pairs.
+bool updateTodo(mongocxx::collection& collection,
+    const string& key, const string& value,
+    const vector<pair<string, string>>& newKeyValues)
+{
+    // Create the query filter to find the document to update
+    auto filter = bsoncxx::builder::stream::document{} << key << value << bsoncxx::builder::stream::finalize;
+
+    // Create the new values for the document
+    bsoncxx::builder::stream::document updated_document{};
+    for (auto& keyValue : newKeyValues)
+    {
+        updated_document << keyValue.first << keyValue.second;
+    }
+
+    auto update_doc = bsoncxx::builder::stream::document{} << "$set" << updated_document << bsoncxx::builder::stream::finalize;
+
+    // Update the document
+    auto result = collection.update_one(filter.view(), update_doc.view());
+
+    // Return true if at least one document was modified
+    return result && result->modified_count() > 0;
+}
+
+// Delete a todo from the given collection based on a specific key-value pair.
+bool deleteTodo(mongocxx::collection& collection, const string& key, const string& value)
+{
+    // Create the query filter to find the document to delete
+    auto filter = bsoncxx::builder::stream::document{} << key << value << bsoncxx::builder::stream::finalize;
+
+    // Delete the document
+    auto result = collection.delete_one(filter.view());
+
+    // Return true if at least one document was deleted
+    return result && result->deleted_count() > 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include "Methods.h"
 
 // ********************************************** Main **********************************************
@@ -11,8 +10,8 @@ int main()
 	mongocxx::client conn{ mongocxx::uri{mongoConnect} };
 	auto collection = conn["TodoRecords"]["TodoCollection"];//get collection from database
 
-	//API endpoint to read all documents
-	CROW_ROUTE(app, "/api/v1/todos")
+	//API endpoint to read all todos
+	CROW_ROUTE(app, "/api/v1/todos").methods(HTTPMethod::GET)
 		([&collection](const request& req) {
 		mongocxx::options::find opts;
 		auto docs = collection.find({}, opts);
@@ -26,7 +25,7 @@ int main()
 		return crow::response{ dto };
 			});
 
-	//API endpoint to insert document from the given json body
+	//API endpoint to insert todo from the given json body
 	CROW_ROUTE(app, "/api/v1/todos").methods(HTTPMethod::POST)
 		([&collection](const request& req) {
 		crow::json::rvalue request_body = json::load(req.body);
@@ -60,6 +59,85 @@ int main()
 			return crow::response(400, "ID already present in the database");
 		}
 			});
+
+	//API endpoint to update document based on the given id in the JSON body
+		CROW_ROUTE(app, "/api/v1/todos").methods(HTTPMethod::PUT)
+		([&collection](const request& req) {
+		crow::json::rvalue request_body = json::load(req.body);
+
+		if (!request_body.has("Id")) {
+			return crow::response(400, "Required key 'Id' missing in request body");
+		}
+
+		std::string id = std::string(request_body["Id"]);
+		bool id_already_present = findTodoRecord(collection, id);
+
+		if (id_already_present) {
+			// ID is present, so update the record
+			 vector<pair<string, string>> updates;
+			 for (auto& item : request_body) {
+				 string key = item.key();
+				 if (key != "Id") {
+					 try {
+						 string value = item.s();
+						 updates.push_back({ key, value });
+					 }
+					 catch (const std::runtime_error&) {
+						 // Not a string, ignore or handle accordingly
+					 }
+				 }
+			 }
+			if (updateTodo(collection, "Id", id, updates)) {
+				return crow::response(200, "Todo Updated Successfully!!");
+			}
+			else {
+				return crow::response(500, "Failed to update Todo");
+			}
+		}
+		else {
+			return crow::response(400, "ID not found in the database");
+		}
+			});
+
+		#undef DELETE
+
+	// endpoint to delete a document based on the given id in the JSON body
+		CROW_ROUTE(app, "/api/v1/todos").methods(HTTPMethod::DELETE)
+		([&collection](const request& req) {
+		crow::json::rvalue request_body = json::load(req.body);
+
+		if (!request_body.has("Id")) {
+			return crow::response(400, "Required key 'Id' missing in request body");
+		}
+
+		std::string id = std::string(request_body["Id"]);
+		if (deleteTodo(collection, "Id", id)) {
+			return crow::response(200, "Todo Deleted Successfully!!");
+		}
+		else {
+			return crow::response(400, "Failed to delete Todo with given ID");
+		}
+			});
+
+		//API endpoint to read a specific document by Id
+		CROW_ROUTE(app, "/api/v1/todos/<string>")
+			([&collection](const string& id) {
+			// Create the query filter based on the provided Id
+			auto filter = bsoncxx::builder::stream::document{} << "Id" << id << bsoncxx::builder::stream::finalize;
+
+			auto maybe_result = collection.find_one(filter.view());
+
+			if (maybe_result) {
+				auto doc = maybe_result->view();
+				crow::json::wvalue dto;
+				dto["todo"] = json::load(bsoncxx::to_json(doc));
+				return crow::response{ dto };
+			}
+			else {
+				return crow::response(404, "Todo not found");
+			}
+				});
+
 
 	//set the port, set the app to run on multiple threads, and run the app
 	app.bindaddr("127.0.0.1").port(8080).multithreaded().run();
